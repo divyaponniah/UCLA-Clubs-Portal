ef8cb4cddd18cd040427adf5aa02a69cb16cb201
Divya Ponniah <dponniah@ucla.edu>
2020-12-05 21:17:06 -0800

    test

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -5,7 +5,7 @@ export default class home extends React.Component {
   render() {
     return (
       <div className="body">
-          body
+          temp
       </div>
     );
   }
--- frontend/src/components/main.js
+++ frontend/src/components/main.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Home from "./home";
 import Login from "./login";
+import signUp from "./signup"
 
 const Main = () => {
   return (
@@ -9,6 +10,7 @@ const Main = () => {
       {/* The Switch decides which component to show based on the current URL.*/}
       <Route exact path="/" component={Home}></Route>
       <Route exact path="/login" component={Login}></Route>
+      <Route exact path="/signup" component={signUp}></Route>
     </Switch>
   );
 };
