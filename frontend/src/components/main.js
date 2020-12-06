ee9c31131ca664ceac1cceb1547d9206c6f6c562
Kate Land <kateqland@gmail.com>
2020-12-06 00:13:45 -0500

    Revert "Merge pull request #2 from kateqland/main"
    This reverts commit 32029e7145b51a02574e253674b26fd3608dd402, reversing
    changes made to 16317682855c6c0fa9a4ee9e7831c9a20e1c35ac.

--- frontend/src/components/main.js
+++ frontend/src/components/main.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Home from "./home";
 import Login from "./login";
-import Clubs from "./clubs";
 
 const Main = () => {
   return (
@@ -10,7 +9,6 @@ const Main = () => {
       {/* The Switch decides which component to show based on the current URL.*/}
       <Route exact path="/" component={Home}></Route>
       <Route exact path="/login" component={Login}></Route>
-      <Route exact path="/clubs" component={Clubs}></Route>
     </Switch>
   );
 };
