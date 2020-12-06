2cdb9d91e1e77c827a8c66533406b0a271870b14
Kate Land <kateqland@gmail.com>
2020-12-06 00:36:24 -0500

    manually merged login and signup pages

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,12 +1,70 @@
 import React from "react";
 import "../styles/layout.css";
+import {Link } from "react-router-dom";
 
 export default class login extends React.Component {
-  render() {
-    return (
-      <div className="body">
-          login
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = { 
+      email: '',
+      password: '', 
+    };
   }
+
+  submitHandler = (event) => {
+    event.preventDefault();
+    alert("Logging In");
+    //if it's valid it'll take you to the home page
+  }
+
+  inputHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({[nam]: val});
+  }
+
+  render() {
+    return (<div className="background">
+    <div className="welcomeTxt">
+      <p>Welcome to the UCLA Clubs Portal</p>
+    </div>
+    <div className="logInPortalTxt">
+      <p>Log in to my portal</p>
+    </div>
+    <form onSubmit={this.submitHandler}>
+      <div className="emailTxt">
+        <p>Email</p>
+      </div>
+      <input
+        type='text'
+        name='email'
+        id='ip1'
+        onChange={this.myChangeHandler}
+      />
+      <div className="passwordTxt">
+        <p>Password</p>
+      </div>
+      <input
+        type='text'
+        name='password'
+        id='ip2'
+        onChange={this.myChangeHandler}
+      />
+      <Link to="/clubs"><button
+        type='submit'
+        id='ip3'>LOG IN</button></Link>
+    </form>
+    <div className="registerText">
+      <p>Don't have an account?</p>
+    </div>
+    <div className="registerButton">
+      <Link to="/signup">
+        <button id='ib1'>
+          Create One.
+        </button>
+      </Link>
+    </div> 
+  </div>
+);
+}
 }
--- frontend/src/components/main.js
+++ frontend/src/components/main.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Home from "./home";
 import Login from "./login";
-import Clubs from "./clubs";
+import SignUp from "./signup"
+import Clubs from "./clubs"
 
 const Main = () => {
   return (
@@ -10,6 +11,8 @@ const Main = () => {
       {/* The Switch decides which component to show based on the current URL.*/}
       <Route exact path="/" component={Home}></Route>
       <Route exact path="/login" component={Login}></Route>
+      <Route exact path="/signup" component={SignUp}></Route>
+      <Route exact path="/signup" component={SignUp}></Route>
       <Route exact path="/clubs" component={Clubs}></Route>
     </Switch>
   );
